1ddd118e9b870bdd077315234809115d3bd3da8b
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-08-23 10:09:32 +0530

    Create pn.java

---
+++ pn.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class pn
+{
+public static void main(String s[])
+{
+Scanner a=new Scanner(System.in);
+int b;
+b=a.nextInt();
+if(b>0)
+{
+System.out.print("POSTIVE");
+}
+else if(b==0)
+{
+System.out.print("ZERO");
+}
+else
+{
+System.out.print("NEGATIVE");
+}}
+}
