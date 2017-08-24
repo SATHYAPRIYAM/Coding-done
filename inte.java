f5933f2f03bf3a8a3925c9fbbf852fbbcafe4a3b
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-08-24 20:58:50 +0530

    Create inte.java

---
+++ inte.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+public class inte
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+String n=sc.nextLine();
+int k=Integer.parseInt(n);
+System.out.println(k);
+}
+}
