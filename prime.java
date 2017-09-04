d11f775565db755edd1751dfff78917f6586b71f
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-09-04 20:25:02 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+class palin
+{
+public static void main(String a[])
+{
+System.out.println("Enter the number");
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+boolean ip=true;
+if(n==1)
+{
+ip=true;
+}
+for(int j=2;j<=n/2;j++)
+{
+if(n%j==0)
+{
+ip=false;
+break;
+}
+}
+if(ip)
+{
+System.out.print(n+"is prime");
+}
+else
+{
+System.out.print(n+"is not prime");
+}
+}}
+
+
