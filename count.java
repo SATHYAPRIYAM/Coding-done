cc4f400da23b802e796179a558a678ebf00a3225
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-09-18 21:58:25 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class pri
+{
+public static void main(String args[])
+{
+int num,i,rev=0;
+System.out.println("Enter the number");
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+while(num!=0)
+{
+i=num%10;
+rev=rev+1;
+num=num/10;
+}
+System.out.println("The count is "+rev);
+}
+}
+
+
