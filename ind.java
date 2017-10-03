fe615ac6b90002579cf0b4f3a361ee3a3bcaa7b0
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-10-03 21:36:13 +0530

    Create ind.java

---
+++ ind.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+class ind
+{
+public static void main(String str[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the array size");
+int n=sc.nextInt();
+int[] a=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}s
+for(int i=0;i<n;i++)
+{
+if(a[i]==i)
+{
+System.out.println("The number"+a[i]+" equals its index"+i);
+}
+else
+{}
+}
+}
+}
