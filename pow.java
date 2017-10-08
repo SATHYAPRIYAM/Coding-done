5a7d8004be21c4cf4569f1b3c5aea2213b9253c8
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-10-08 22:49:46 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.Math.pow;
+class pow
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+double n=sc.nextInt();
+System.out.println("Enter the power");
+double a=sc.nextInt();
+double k=pow(n,a);
+System.out.println("The power is"+k);
+}
+}
