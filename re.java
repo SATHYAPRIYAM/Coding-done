1869badfba18a7acc559002528a90175d81b20c0
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-10-09 20:58:43 +0530

    Update re.java

--- re.java
+++ re.java
@@ -6,6 +6,7 @@ public static void main(String s[])
 {
 String k=new String(s[0]);
 String rev=new StringBuffer(k).reverse().toString();
+System.out.println(rev);
 String news=new String();
 news=rev.replaceAll("[aeiouAEIOU]", "");
 System.out.println(news);
