5200251379cbda84907c63eb4b3c27c50743f880
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-10-13 20:31:19 +0530

    Update pri.java

--- pri.java
+++ pri.java
@@ -9,17 +9,15 @@ System.out.print("Enter the range");
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
 boolean flag=true;
-for(i=1;i<=n;i++)
-{
-if(i==1 || i==2)
+if(n==1 || n==2)
 {
 flag=true;
 }
 else
 {
-for (int j=2;j<=i/2;j++)
+for (int j=2;j<=n/2;j++)
 {
-if(i%j==0)
+if(n%j==0)
 {
 flag=false;
 }
@@ -29,7 +27,7 @@ flag=true;
 }
 }
 }
-System.out.print(i+"is number"+flag+"\n");
+System.out.print(n+"is number"+flag+"\n");
 }
 }
-}
+
