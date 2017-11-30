ace1173c7e8408885bbca91da62aabda8ed109bb
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-11-30 20:31:18 +0530

    Create oddi.java

---
+++ oddi.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class oddi
+{
+public static void main(String str[])
+{
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int n=sc.nextInt();
+for(int i=m;i<=n;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+else
+{}
+}}
+}
