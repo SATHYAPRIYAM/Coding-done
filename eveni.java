32c2bdae8703936361ec1b8f487d8ce19585f5d8
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-11-30 20:36:32 +0530

    Create eveni.java

---
+++ eveni.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class eveni
+{
+public static void main(String str[])
+{
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int n=sc.nextInt();
+for(int i=m;i<n-1;i++)
+{
+if((i+1)%2==0)
+{
+System.out.println(i+1);
+}
+else
+{}
+}}
+}
