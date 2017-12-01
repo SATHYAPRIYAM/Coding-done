b49b970b1d526a2dd2fc471a7f2e0b560d3e7f5b
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2017-12-01 20:57:05 +0530

    Create uniq.java

---
+++ uniq.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class uniq {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		a[i]=sc.nextInt();
+		}
+		HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+		for(int i=0;i<n;i++)
+		{
+			if(map.containsKey(a[i]))
+			{
+				int v=map.get(a[i]);
+				map.put(a[i], v+1);
+			}
+			else
+			{
+				map.put(a[i], 1);
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(map.get(a[i])==1)
+			{
+				System.out.println(a[i]);
+			}
+			
+		}
+		sc.close();
+		}
+
+}
