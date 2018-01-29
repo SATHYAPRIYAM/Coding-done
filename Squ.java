f7c80d436c4e1e599e4f713ae12798a85f070d71
SATHYAPRIYAM <31247730+SATHYAPRIYAM@users.noreply.github.com>
2018-01-29 20:32:54 +0530

    Create Squ.java

---
+++ Squ.java
@@ -0,0 +1,23 @@
+package first;
+
+import java.util.Scanner;
+
+public class Squ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
+		int sum=0,n;
+		while(number>0)
+		{
+		n=number%10;
+		sum=(n*n)+sum;
+		number=number/10;
+		}
+		System.out.println(sum);
+		sc.close();
+
+	}
+
+}
